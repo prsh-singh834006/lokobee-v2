8ff2ae2b04174f2d528d4012567afa23e3eb92df
Prashant Kumar <prashantsingh619619@gmail.com>
2019-11-03 18:37:08 +0530

    [master] App changed

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code>
         </p>
         <Apple />
         <Dummy />
